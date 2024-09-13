f6b24e0432f5a73cd217abf8e83726d3a7878ce9
Давид Уланович <david20222009@gmail.com>
2024-09-13 22:03:45 +0300

    commit publish main convas code

---
+++ Canva.js
@@ -0,0 +1,12 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+ctx.fillRect(100, 30, 50, 50);
+ctx.fillRect(120, 80, 10, 10);
+ctx.fillRect(95, 90, 60, 70);
+ctx.fillRect(100, 160, 10, 100);
+ctx.fillRect(140, 160, 10, 100);
+ctx.fillRect(90, 90, 10, 10);
+ctx.fillRect(150, 90, 10, 10);
+ctx.fillRect(80, 90, 10, 50);
+ctx.fillRect(160, 90, 10, 50);
